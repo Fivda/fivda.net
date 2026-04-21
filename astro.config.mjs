b0cf664b24f3b006dbb8acf248edf92c39bc72cc
Viviane <dessaintviviane@gmail.com>
2026-04-21 22:53:53 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+      locales: ["fr", "en"],
+      defaultLocale: "en",
+      fallback: {
+          fr: "en"
+      },
+      routing: {
+          fallbackType: "rewrite"
+      }
+	},
+
+  vite: {
+      plugins: [tailwindcss()],
+	},
+
+  integrations: [react()],
+});
